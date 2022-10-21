5d0ab4e55b5a94ef79c549fd54572fe650daaa00
Sid Premkumar <sid.premkumar@gmail.com>
2022-10-21 11:08:27 -0400

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'rb') as f:
 
 setup(
     name='pd2slack',
-    version=0.6,
+    version=0.61,
     description='Syncs PD On-call to Slack User Group',
     long_description = long_description,
     long_description_content_type='text/markdown',
